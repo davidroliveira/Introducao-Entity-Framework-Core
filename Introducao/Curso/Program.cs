6b5334c36674950e5a3b91fc768180146c12949b
THEOS\david.oliveira <david.theos@outlook.com.br>
2021-10-26 18:26:32 -0300

    Aplicando migração (pela aplicação) ou pelos comandos
     - PM > Script-Migration -o PrimeiraMigracao.SQL -Verbose
     - PM > Update-Database -v

--- Introducao/Curso/Program.cs
+++ Introducao/Curso/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace CursoEFCore
 {
@@ -6,6 +7,10 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
+            using var db = new Data.ApplicationContext();
+            
+            db.Database.Migrate(); //Não indicado para usar em produção
+
             Console.WriteLine("Hello World!");
         }
     }
