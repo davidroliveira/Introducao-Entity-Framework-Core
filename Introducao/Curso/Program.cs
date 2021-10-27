ea53ca9f1ffbb40ee81243643f7f6a41f686c596
David <davidroliveira@gmail.com>
2021-10-26 21:00:16 -0300

    Migrações pendentes
    Comandos:
    PM> Add-Migration AdicionarEmail -Verbose

--- Introducao/Curso/Program.cs
+++ Introducao/Curso/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace CursoEFCore
 {
@@ -8,8 +9,11 @@ namespace CursoEFCore
         static void Main(string[] args)
         {
             using var db = new Data.ApplicationContext();
-            
-            db.Database.Migrate(); //Não indicado para usar em produção
+
+            //db.Database.Migrate(); //Não indicado para usar em produção
+            var existe = db.Database.GetPendingMigrations().Any();
+            if (existe)            
+                Console.WriteLine("Existem Migrações pendentes");            
 
             Console.WriteLine("Hello World!");
         }
